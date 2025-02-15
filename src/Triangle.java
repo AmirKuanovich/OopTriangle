37b84665f3592c783d84f5a7c2eab7606a8c5440
Amir Dev <altymyshevamir@gmail.com>
2025-02-15 14:02:39 +0600

    git push

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Triangle {
+    double a;
+    double b;
+    double c;
 
-public class Trinagle {
+    public double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        double s = p * (p - a) * (p - b) * (p - c);
+        double sqrt = Math.sqrt(s);
+        long round = Math.round(sqrt);
+        return round;
+    }
+
 }
